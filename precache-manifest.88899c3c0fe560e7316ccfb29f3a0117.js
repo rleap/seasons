00db4d022eba50904ef2911089faffcde53ce071
Robert Leap <robertleap@gmail.com>
2019-06-17 22:54:57 -0700

    Updates

---
+++ precache-manifest.88899c3c0fe560e7316ccfb29f3a0117.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0e0127437f38bc9b07c546d994ab10",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "646d237a04d89268285d",
+    "url": "/seasons/static/css/main.c5c5e061.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/seasons/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "646d237a04d89268285d",
+    "url": "/seasons/static/js/main.ca4f67ef.chunk.js"
+  },
+  {
+    "revision": "808fed5ac1e88b432622",
+    "url": "/seasons/static/js/runtime~main.fbc5b39b.js"
+  }
+]);
